98d6b994a089194558517a48f09fef7d6f6641e0
ramraj <cramraj8@gmail.com>
2017-10-14 11:27:09 -0400

    Updated data_providers script

--- data_providers.py
+++ data_providers.py
@@ -1,21 +1,28 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import pandas as pd
 
 
+DATA_FILE = ['./Brain_Integ_X.csv', './Brain_Integ_Y.csv']
+
 def data_providers(data_file=['./Brain_Integ_X.csv', './Brain_Integ_Y.csv']):
-    """
-    This function reads the data file and extracts the features and labelled
-    values.
-    Then according to that patient is dead, only those observations would be
-    taken care
-    for deep learning trainig.
+    """The function for reading datasets.
+
+    This function reads the features, lable datasets separately and filters out
+    only the dead patients' respective observations for further processings.
+
   Args:
-    data_file: list of strings representing the paths of input files. Here the input features and ground truth values are separated in 2 files.
+    data_file: list of strings representing the paths for input files.
+    Here the input features and input ground truth values are provided separately.
+
   Returns:
-    `Numpy array`, extracted feature columns and label column.
+    `Numpy array`, extracted feature matrix and label column.
+
   Example:
     >>> read_dataset()
     ( [[2.3, 2.4, 6.5],[2.3, 5.4,3.3]], [12, 82] )
+
     """
 
     data_feed = pd.read_csv(data_file[0], skiprows=[0], header=None)
@@ -28,6 +35,7 @@ def data_providers(data_file=['./Brain_Integ_X.csv', './Brain_Integ_Y.csv']):
     data = data_feed.values
     data = np.float32(data)
 
+    #Filtering only the dead patients for survival analysis
     censored_survival = survival[censored == 1]
     censored_data = data[censored == 1]
 
@@ -38,7 +46,7 @@ def data_providers(data_file=['./Brain_Integ_X.csv', './Brain_Integ_Y.csv']):
     print('Shape of Y : ', y.shape)
     return (x, y)
 
+
 if __name__ == '__main__':
 
-    data_file = ['./Brain_Integ_X.csv', './Brain_Integ_Y.csv']
-    data_x, data_y = data_providers(data_file)
+    data_x, data_y = data_providers(DATA_FILE)
