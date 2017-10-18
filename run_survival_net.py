4f8230eddfd3abb3250aa281bbd21dac604aed24
ramraj <cramraj8@gmail.com>
2017-10-18 02:24:04 -0400

    Made only one main script there

--- run_survival_net.py
+++ run_survival_net.py
@@ -1,120 +1,133 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import, division, print_function
-import tensorflow as tf
 import numpy as np
 import data_providers
-from sklearn.utils import shuffle
+import visualize
+import dnn_model
 from sklearn.model_selection import train_test_split
 from tensorflow.contrib.framework.python.ops.variables import get_or_create_global_step
 from tensorflow.python.platform import tf_logging as logging
-from tensorflow.python.framework import ops
+import tensorflow as tf
+
+
+flags = tf.app.flags
+flags.DEFINE_string('x_data', './Brain_Integ_X.csv',
+                    'Directory file with features-data.')
+flags.DEFINE_string('y_data', './Brain_Integ_Y.csv',
+                    'Directory file with label-values.')
+flags.DEFINE_string('ckpt_dir', './ckpt_dir/',
+                    'Directory for checkpoint files.')
+flags.DEFINE_float('split_ratio', 0.2,
+                   'Split ratio for test data.')
+flags.DEFINE_float('lr_decay_rate', 0.9,
+                   'Learning decaying rate.')
+flags.DEFINE_float('beta', 0.01,
+                   'Regularizing constant.')
+flags.DEFINE_float('dropout', 0.6,
+                   'Drop out ratio.')
+flags.DEFINE_float('init_lr', 0.001,
+                   'Initial learning rate.')
+flags.DEFINE_integer('batch_size', 100,
+                     'Batch size.')
+flags.DEFINE_integer('n_epochs', 500,
+                     'Number of epochs.')
+flags.DEFINE_integer('n_classes', 1,
+                     'Number of classes in case of classification.')
+flags.DEFINE_integer('display_step', 100,
+                     'Displaying step at training.')
+flags.DEFINE_integer('n_layers', 3,
+                     'Number of layers.')
+flags.DEFINE_integer('n_neurons', 500,
+                     'Number of Neurons.')
+
+FLAGS = flags.FLAGS
 slim = tf.contrib.slim
+split = train_test_split
 
 
-def run_survivalnet(user_params, beta=0.01, dropout_rate=0.6,
-                    hidden_layers=[500, 500, 500],
-                    init_lr=0.0002):
-    """The function for model training.
+def main(args):
+    """The function for TF-Slim DNN model training.
 
-    This function receives user_provided_params and hyper_parameter values
-    as function-arguments. Then it creates the tensorflow model-graph, defines
-    loss and optimizer. Finally creates a training loop and saves the results
-    and logs in the sub-directory.
+    This function receives user-given parameters as gflag arguments. Then it
+    creates the tensorflow model-graph, defines loss and optimizer. Finally,
+    creates a training loop and saves the results and logs in the sub-directory.
 
     Args:
-        user_params: This is a python-dict data-type, containing user-changable paramers.
-        beta: A float value represents regularizing constant.
-        dropout_rate: A 'float' value represents drop out ratio.
-        hidden_layers: A python-list with 'int' elements, each represents layer width.
-        init_lr: A 'float' value represents 'initial learning rate'.
+        args: This brings all gflags given user inputs with default values.
 
     Returns:
         None
-
     """
 
-    #Extracting user_given_parameters from function argument
-    n_epochs = user_params['n_epochs']
-    split_ratio = user_params['split_ratio']
-    batch_size = user_params['batch_size']
-    lr_decay_rate = user_params['lr_decay_rate']
-    n_classes = user_params['n_classes']
-    data_file = user_params['data_file']
-    ckpt_dir = user_params['ckpt_dir']
+    data_x, data_y = data_providers.data_providers(FLAGS.x_data, FLAGS.y_data)
 
-    data_x, data_y = data_providers.data_providers(data_file)
-    data_x, data_y = shuffle(data_x, data_y, random_state=1)
-    x_train, x_test, y_train, y_test = train_test_split(data_x, data_y,
-                                                        test_size=split_ratio,
-                                                        random_state=420)
+    # split the train and test data
+    x_train, x_test, y_train, y_test = split(data_x, data_y,
+                                             test_size=FLAGS.split_ratio,
+                                             random_state=420)
 
     n_obs = x_train.shape[0]
     n_features = x_train.shape[1]
 
-    #TF-Graph creation
+    # Start building the graph
     tf.reset_default_graph()
     with tf.Graph().as_default() as graph:
         logging.set_verbosity(tf.logging.INFO)
 
-        if not tf.gfile.Exists(ckpt_dir):
-            tf.gfile.MakeDirs(ckpt_dir)
+        if not tf.gfile.Exists(FLAGS.ckpt_dir):
+            tf.gfile.MakeDirs(FLAGS.ckpt_dir)
 
-        n_batches = n_obs / batch_size
-        decay_steps = int(n_epochs * n_batches)
+        n_batches = n_obs / FLAGS.batch_size
+        decay_steps = int(FLAGS.n_epochs * n_batches)
 
-        #Create the model
-        def multilayer_nn_model(inputs, hidden_layers, n_classes, beta,
-                                scope="deep_regression_model"):
+        x_batch, y_batch = tf.train.shuffle_batch([x_train, y_train],
+                                                  batch_size=FLAGS.batch_size,
+                                                  capacity=50000,
+                                                  min_after_dequeue=10000,
+                                                  num_threads=1,
+                                                  allow_smaller_final_batch=True)
 
-            with tf.variable_scope(scope, 'deep_regression', [inputs]):
-                end_points = {}
-                with slim.arg_scope([slim.fully_connected],
-                                    activation_fn=tf.nn.relu,
-                                    weights_regularizer=slim.l2_regularizer(beta)):
-                    net = slim.stack(inputs,
-                                     slim.fully_connected,
-                                     hidden_layers,
-                                     scope='fc')
-                    end_points['fc'] = net
-                    predictions = slim.fully_connected(net, n_classes,
-                                                       activation_fn=None,
-                                                       scope='prediction')
-                    end_points['out'] = predictions
-                    return predictions, end_points
-
-        #Predict the values from deep neural network.
-        pred, end_points = multilayer_nn_model(x_train, hidden_layers,
-                                               n_classes, beta)
+        # Create the model and pass the input values batch by batch
+        hidden_layers = [FLAGS.n_neurons] * FLAGS.n_layers
+        pred, end_points = dnn_model.multilayer_nn_model(x_batch,
+                                                         hidden_layers,
+                                                         FLAGS.n_classes,
+                                                         FLAGS.beta)
 
         global_step = get_or_create_global_step()
 
-        lr = tf.train.exponential_decay(learning_rate=init_lr,
+        lr = tf.train.exponential_decay(learning_rate=FLAGS.init_lr,
                                         global_step=global_step,
                                         decay_steps=decay_steps,
-                                        decay_rate=lr_decay_rate,
+                                        decay_rate=FLAGS.lr_decay_rate,
                                         staircase=True)
 
-        loss = tf.losses.mean_squared_error(tf.squeeze(pred), y_train)
+        # Define loss
+        loss = tf.losses.mean_squared_error(tf.squeeze(pred), y_batch)
+        tf.losses.add_loss(loss)
+        total_loss = tf.losses.get_total_loss()
+
+        tf.summary.scalar('loss', total_loss)
 
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr)
 
-        #Create slim-train model
+        # create the back-propagation object
         train_op = slim.learning.create_train_op(
-            loss,
+            total_loss,
             optimizer,
             clip_gradient_norm=4,   # Gives quick convergence
             check_numerics=True,
             summarize_gradients=True)
 
-        #Create the slim-training-loop
+        # create the training loop
         final = slim.learning.train(
             train_op,
-            ckpt_dir,
+            FLAGS.ckpt_dir,
             log_every_n_steps=1,
             graph=graph,
             global_step=global_step,
-            number_of_steps=n_epochs,
+            number_of_steps=FLAGS.n_epochs,
             save_summaries_secs=20,
             startup_delay_steps=0,
             saver=None,
@@ -124,23 +137,4 @@ def run_survivalnet(user_params, beta=0.01, dropout_rate=0.6,
 
 
 if __name__ == '__main__':
-
-    user_params = {
-        'n_epochs': 500,
-        'split_ratio': 0.2,
-        'batch_size': 100,
-        'lr_decay_rate': 0.9,
-        'n_classes': 1,
-        'data_file': ['./Brain_Integ_X.csv', './Brain_Integ_Y.csv'],
-        'ckpt_dir': './ckpt_dir/'
-    }
-
-    hyper_params = {
-        'beta': 0.01,
-        'dropout_rate': 0.6,
-        'hidden_layers': [500, 500, 500],
-        'init_lr': 0.001
-    }
-
-    run_survivalnet(user_params, **hyper_params)
-    #No returns becasue output and log files would be saved into './ckpt_dir/' directory.
+    tf.app.run()
